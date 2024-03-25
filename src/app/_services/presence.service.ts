1aff2aa30161569e69adf80ad2a48fb3355bf57b
Kritartha1 <kritarthanathofficial@gmail.com>
2024-03-25 13:19:11 +0000

    new changes

--- src/app/_services/presence.service.ts
+++ src/app/_services/presence.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
 import { environment } from 'src/environments/environment.development';
 import { User } from '../models/User';
+import { BehaviorSubject } from 'rxjs';
 
 
 @Injectable({
@@ -11,6 +12,10 @@ export class PresenceService {
 
   hubUrl=environment.hubsBaseUrl;
   private hubConnection?:HubConnection;
+  private onlineUsersSource=new BehaviorSubject<string[]>([]);
+
+  onlineUsers$=this.onlineUsersSource.asObservable();
+
   constructor() { }
 
 
@@ -31,6 +36,10 @@ export class PresenceService {
 
     this.hubConnection.on('UserIsOffline',username=>{
       console.log(`${username} has disconnected`)
+    })
+
+    this.hubConnection.on('GetOnlineUsers',usernames=>{
+      this.onlineUsersSource.next(usernames);
     })
 
   }
